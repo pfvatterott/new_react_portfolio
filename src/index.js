d0ca6aa0194554d09cfe8e556b8f7f24018b931a
Paul Vatterott <pfvatterott@gmail.com>
2022-06-16 08:52:49 -0600

    creating grid

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
 import Home from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { motion } from "framer-motion"
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 function Home() {
   return (
     <div>
-        <h2>working</h2>
+      <Container className='align-items-center justify-content-center d-flex'>
+        <Row>
+          <Col>
+            <h2>working</h2>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
