b0cccaca7820f5b73675a933fc1103078916bd73
Paul Vatterott <pfvatterott@gmail.com>
2022-06-22 12:26:08 -0600

    Option placements

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from "framer-motion"
+import { motion, motionValue, useMotionValue } from "framer-motion"
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquare } from '@fortawesome/free-solid-svg-icons'
@@ -9,13 +9,27 @@ import "./style.css"
 function Home() {
 
 
+  function test(x) {
+    console.log('x-axis ' + x.x)
+    console.log('y-axis ' + x.y)
+  }
+
   return (
     <div>
       <Container className='container-center'>
+        <Row className='top-row-options'>
+          <Col className="left-option">
+            <h3>Test</h3>
+          </Col>
+          <Col className="right-option">
+            <h3>Test</h3>
+          </Col>
+        </Row>
         <Row>
           <Col>
             <motion.div
               drag
+              onDragEnd={(x) => test(x)}
               dragSnapToOrigin={true}
               dragConstraints={{
                 top: -50,
@@ -32,6 +46,14 @@ function Home() {
             >
               <FontAwesomeIcon icon={faSquare} className='square'/>
             </motion.div>
+          </Col>
+        </Row>
+        <Row className='bottom-row-options'>
+          <Col className='left-option'>
+            <h3>Test</h3>
+          </Col>
+          <Col className='right-option'>
+            <h3>Test</h3>
           </Col>
         </Row>
       </Container>
