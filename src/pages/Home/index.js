5dfbe81fbc103a099ef85d8c2be66f4635fdd205
Paul Vatterott <pfvatterott@gmail.com>
2022-06-23 08:58:33 -0600

    added tooltip

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
-import { React } from 'react'
-import { motion } from "framer-motion"
+import { React, useState } from 'react'
+import { motion, AnimatePresence } from "framer-motion"
 import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquare } from '@fortawesome/free-solid-svg-icons'
+import { faSquare, faIcons, faAddressCard, faComputer, faRobot } from '@fortawesome/free-solid-svg-icons'
 import "./style.css"
 
 
 function Home() {
+  const [hoverTitle, setHoverTitle] = useState('')
+  const [isVisible, setIsVisible] = useState(false)
 
   function getCubeLocation(x) {
     let cube = document.getElementById("drag-cube");
@@ -16,6 +18,10 @@ function Home() {
     let topRightOption = document.getElementById("top-right-option");
     let BottomLeftOption = document.getElementById("bottom-left-option");
     let BottomRightOption = document.getElementById("bottom-right-option");
+    console.log('cubex ' + cubeX)
+    console.log('cubeY ' + cubeY)
+    console.log(topLeftOption.getBoundingClientRect())
+    console.log(cube.getBoundingClientRect())
 
     // if to the left
     if (((cubeX / topLeftOption.getBoundingClientRect().x < 1.1) && (cubeX / topLeftOption.getBoundingClientRect().x > .9)) || ((cubeX - topLeftOption.getBoundingClientRect().x < 40) && (cubeX - topLeftOption.getBoundingClientRect().x > -40))) {
@@ -47,19 +53,50 @@ function Home() {
     
   }
 
+  function setIsShown(x) {
+    setHoverTitle(x)
+    setIsVisible(true)
+  }
+
+  function setNotShown() {
+    setHoverTitle('')
+    setIsVisible(false)
+  }
+
   return (
     <div>
-      <Container className='container-center'>
-        <Row className='top-row-options'>
-          <Col className="left-option">
-            <h3 id='top-left-option'>Test</h3>
-          </Col>
-          <Col className="right-option">
-            <h3 id='top-right-option'>Test</h3>
+      <Container className='top-space-container'>
+        <Row>
+          <Col xs="12" className='text-center'>
+            <AnimatePresence>
+              {isVisible && (
+              <motion.div
+                  initial={{ opacity: 0, scale: 0.75 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0 }}
+              >
+              <h2 className='text-center' id="option-description">{hoverTitle}</h2>
+              </motion.div>
+              )}
+            </AnimatePresence>
           </Col>
         </Row>
+      </Container>
+      <Container className='container-center text-center'>
         <Row>
-          <Col>
+          <Col md="3"></Col>
+          <Col xs="6" md="3">
+            <FontAwesomeIcon icon={faRobot} id='top-left-option' className='icon-option' onMouseEnter={() => setIsShown('about me')} onMouseLeave={() => setNotShown()}/>
+          </Col>
+          <Col xs="6" md="3">
+            <FontAwesomeIcon icon={faComputer} id='top-right-option' className='icon-option'onMouseEnter={() => setIsShown('languages & skills')} onMouseLeave={() => setNotShown()}/>
+          </Col>
+          <Col md="3"></Col>
+        </Row>
+        <Row><br></br></Row>
+        <Row>
+          <Col xs="3"></Col>
+          <Col xs="6">
             <motion.div
               drag
               id='drag-cube'
@@ -72,26 +109,27 @@ function Home() {
                 bottom: 50,
               }}
               dragElastic={.8}
-              animate={{
-                scale: [1, 2, 2, 1, 1],
-                rotate: [0, 0, 270, 270, 0],
-                borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-              }}
+              
               whileHover={{ scale: 1.1 }}
             >
               <FontAwesomeIcon icon={faSquare} className='square'/>
             </motion.div>
           </Col>
+          <Col xs="3"></Col>
         </Row>
-        <Row className='bottom-row-options'>
-          <Col className='left-option'>
-            <h3 id='bottom-left-option'>Test</h3>
+        <Row><br></br></Row>
+        <Row>
+          <Col md="3"></Col>
+          <Col xs="6" md="3">
+            <FontAwesomeIcon icon={faIcons} id='bottom-left-option' className='icon-option' onMouseEnter={() => setIsShown('projects')} onMouseLeave={() => setNotShown()}/>
           </Col>
-          <Col className='right-option'>
-            <h3 id='bottom-right-option'>Test</h3>
+          <Col xs="6" md="3">
+            <FontAwesomeIcon icon={faAddressCard} id='bottom-right-option' className='icon-option' onMouseEnter={() => setIsShown('contact')} onMouseLeave={() => setNotShown()}/>
           </Col>
+          <Col md="3"></Col>
         </Row>
       </Container>
+      <Container className='bottom-space-container'></Container>
     </div>
   )
 }
