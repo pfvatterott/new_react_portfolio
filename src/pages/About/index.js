c731bb8e5e8c63d3473a3488b9b68bc5b1c5e25c
Paul Vatterott <pfvatterott@gmail.com>
2022-06-27 18:10:41 -0600

    continued work on about section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,7 @@
 import { React, useState } from 'react'
 import { motion, AnimatePresence, useMotionValue, useTransform } from "framer-motion"
-import { Container, Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquare, faIcons, faAddressCard, faComputer, faRobot } from '@fortawesome/free-solid-svg-icons'
+import { Container, Row, Col, Collapse } from 'react-bootstrap';
+import ProfilePicture from "../../assets/profile-picture.jpg"
 import "./style.css"
 
 
@@ -29,11 +28,40 @@ function About() {
            transition={{duration: 1.5}}
            exit={{ opacity: 0 }}
          >
-          <h3>test</h3>
+          <Container id='top-space-container-about' className='d-none d-sm-block'></Container>
+          <Container id='middle-space-container-about'>
+            <Row>
+              <Col xs="12" md="6">
+                <Row>
+                  <Col>
+                    <h1>Paul Vatterott</h1>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <h2>Full-Stack Web Developer</h2>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <br></br>
+                    <p>I am a Full Stack Web Developer Bootcamp graduate with a passion for problem solving, working with others, learning, and making products I’m proud of. My natural curiosity paired with a strong work ethic means I am always looking to improve my skills.</p>
+
+                    <p>Through past experiences leading countless outdoors expeditions I have acquired the ability to work with and manage teams of varying sizes. I naturally like to take the reins to solve problems while also feeling content to step back and learn from others.</p>
+
+                    <p>I am currently employed as a Sr. Technical Support Specialist at ClickUp where I have developed a deep understanding of ClickUp’s software. My responsibilities include finding bugs, assisting enterprise clients with technical issues, and helping colleagues when they have questions about the product.</p>
+                  </Col>
+                </Row>
+              </Col>
+              <Col xs="12" md="6" className='text-center'>
+                <img src={ProfilePicture} id="profile-pic"></img>
+              </Col>
+            </Row>
+          </Container>
+          <Container id='bottom-space-container-about'></Container>
           </motion.div>
         )}
       </AnimatePresence>
-
     </motion.div>
   )
 }
